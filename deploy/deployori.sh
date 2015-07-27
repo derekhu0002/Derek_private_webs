29ceb030d3a164d7e72609e9d730208b95cd2064
derekhu <251877979@qq.com>
2015-07-27 23:17:41 +0800

    modify dep

--- deploy/deployori.sh
+++ deploy/deployori.sh
@@ -3,6 +3,6 @@ rm -rf /opt/apache-tomcat-7.0.61/webapps
 rm -rf /opt/apache-tomcat-7.0.61/resoures
 rm -rf /opt/apache-tomcat-7.0.61/conf
 rm -rf /opt/apache-tomcat-7.0.61/lib/libs
-python /home/arrdudataftp/SVNRepos/deploy/deloydynmainpy.py
+python /home/arrdudataftp/derekprivateweb_deployment/deloydynmainpy.py
 sudo sh /opt/apache-tomcat-7.0.61/bin/shutdown.sh 
 sudo sh /opt/apache-tomcat-7.0.61/bin/startup.sh 
